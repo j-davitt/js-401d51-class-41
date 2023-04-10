50202ba24f7a21b919e67a0c92edee36268e548b
Joe Davitt <joseph.j.davitt@gmail.com>
2023-04-10 21:07:20 +0000

    initial

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [contacts, setContacts] = useState([]);
+  // console.log('contacts', contacts[0]);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      console.log('status', status);
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync();
+        setContacts(data);
+        // if (data.length > 0) {
+        //   const contact = data[0];
+        //   console.log(contact);
+        // }
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!!!!</Text>
+      <FlatList 
+        data={contacts}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
