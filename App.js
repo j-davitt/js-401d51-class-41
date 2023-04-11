8c8a1042568e1a5e8d9583f6cc70268e230303a9
Joe Davitt <joseph.j.davitt@gmail.com>
2023-04-11 01:53:35 +0000

    add chart

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
+import { LineChart } from 'react-native-chart-kit';
 
 export default function App() {
   const [{ x, y, z }, setData] = useState({
@@ -18,6 +19,29 @@ export default function App() {
 
   const _slow = () => Accelerometer.setUpdateInterval(1000);
   const _fast = () => Accelerometer.setUpdateInterval(16);
+
+  const [chartX, setChartX] = useState(['x']);
+  const [chartY, setChartY] = useState([0]);
+
+  // let chartX = ['x'];
+  // let chartY = [0];
+
+  // const chartInterval = () => {
+  //   setInterval(() => {
+  //     chartX.push('x');
+  //     chartY.push(x);
+  //     console.log('chartX: ', chartX);
+  //     console.log('chartY: ', chartY);
+  //   }, 600);
+  // };
+
+  // const startChart = () => {
+  //   chartInterval();
+  // };
+
+  // const stopChart = () => {
+  //   clearInterval(chartInterval);
+  // };
 
   const _subscribe = () => {
     setSubscription(
@@ -41,6 +65,10 @@ export default function App() {
     if (x > x1) {
       setData1({ x1: x, });
       console.log('x1: ', x1)
+      chartX.push('x');
+      chartY.push(x);
+      // console.log('chartX: ', chartX);
+      // console.log('chartY: ', chartY);
     }
   }, [{ x }]);
 
@@ -66,10 +94,52 @@ export default function App() {
       <Text style={styles.text}>x: {x}</Text>
       <Text style={styles.text}>y: {y}</Text>
       <Text style={styles.text}>z: {z}</Text>
+      {/* <Button title="Start" onPress={startChart} />
+      <Button title="Stop" onPress={stopChart} /> */}
+      <MyLineChart
+        chartX={chartX}
+        chartY={chartY}
+      />
 
     </View>
   );
 }
+
+const MyLineChart = (props) => {
+
+  return (
+    <>
+      <Text style={styles.text}>Line Chart</Text>
+      <LineChart
+        data={{
+          labels: props.chartX,
+          datasets: [
+            {
+              data: props.chartY,
+              strokeWidth: 2,
+            },
+          ],
+        }}
+        width={325}
+        height={220}
+        chartConfig={{
+          backgroundColor: '#1cc910',
+          backgroundGradientFrom: '#eff3ff',
+          backgroundGradientTo: '#efefef',
+          decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
